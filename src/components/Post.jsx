a07721e300d2f894ad890f2dddc25a97a75268fa
Chinmay Kumar Sethi <chinmay2003kumar@gmail.com>
2026-03-18 11:07:43 +0530

    reactions

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,8 @@
+import { useContext } from "react";
 import { AiFillDelete } from "react-icons/ai";
+import PostList from "./PostList";
 const Post = ({ post }) => {
+  const { deletepost } = useContext(PostList);
   return (
     <div className="card post-card" style={{ width: "30rem" }}>
       <div className="card-body">
@@ -14,7 +17,7 @@ const Post = ({ post }) => {
           <span class="badge text-bg-primary hashtag">{tag}</span>
         ))}
         <div class="alert alert-success reactions" role="alert">
-          A simple success alert—check it out!
+          This post has been reacted by {post.reactions} people
         </div>
       </div>
     </div>
