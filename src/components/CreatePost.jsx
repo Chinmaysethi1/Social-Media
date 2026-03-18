40d2c7559c7733f8b1e44d149b8c6ee53d1d52f6
Chinmay Kumar Sethi <chinmay2003kumar@gmail.com>
2026-03-18 20:24:36 +0530

    adding refs

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,4 +1,12 @@
+import { useRef } from "react";
+
 const Creataposts = () => {
+  const userId = useRef();
+  const postTile = useRef();
+  const postBody = useRef();
+  const reactions = useRef();
+  const tags = useRef();
+
   return (
     <form className="create-post">
       <div className="mb-3">
@@ -7,6 +15,7 @@ const Creataposts = () => {
         </label>
         <input
           type="text"
+          ref={userId}
           className="form-control"
           id="USerId"
           placeholder="Your USsr name"
@@ -18,6 +27,7 @@ const Creataposts = () => {
         </label>
         <input
           type="text"
+          ref={postTile}
           className="form-control"
           id="title"
           placeholder="How are you feeling today!!"
@@ -30,6 +40,7 @@ const Creataposts = () => {
         <textarea
           type="text"
           rows="5"
+          ref={postBody}
           className="form-control"
           id="body"
           placeholder="Tell us more about you!!"
@@ -41,14 +52,27 @@ const Creataposts = () => {
         </label>
         <input
           type="text"
+          ref={reactions}
           className="form-control"
           id="reaction"
           placeholder="How many people reacted!"
         />
       </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Ennnter your hashtags:
+        </label>
+        <input
+          type="text"
+          ref={tags}
+          className="form-control"
+          id="tags"
+          placeholder="Please enter the tags using space "
+        />
+      </div>
 
       <button type="submit" className="btn btn-primary">
-        Submit
+        POST
       </button>
     </form>
   );
